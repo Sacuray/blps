59c98b73e3b1c3960163aea3121a0fd3a4abceb7
Sacuray <311721@niuitmo.ru>
2024-03-30 01:03:00 +0300

    version 0.0.1

---
+++ create_tables.sql
@@ -0,0 +1,21 @@
+CREATE TYPE engineType AS ENUM(
+    'DIESEL',
+    'PETROL'
+);
+
+CREATE TYPE wheelType AS ENUM(
+    'LEFT',
+    'RIGHT'
+);
+CREATE TABLE car (
+    id serial PRIMARY KEY,
+    model VARCHAR(64) NOT NULL,
+    adNumber BIGINT NOT NULL UNIQUE ,
+    yearOfRelease date NOT NULL,
+    colour varchar(64),
+    wheel wheelType,
+    engine engineType,
+    engineVolume FLOAT,
+    mileage BIGINT,
+    accident BOOLEAN
+);
