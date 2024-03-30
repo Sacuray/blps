35bc301e9233069a9ba573b2aa90327b237fa522
Lasat <61060252+Himezon@users.noreply.github.com>
2024-03-30 11:02:41 +0300

    sql update

--- create_tables.sql
+++ create_tables.sql
@@ -1,22 +1,13 @@
 DROP TABLE IF EXISTS car CASCADE;
 
-CREATE TYPE engineType AS ENUM(
-    'DIESEL',
-    'PETROL'
-);
-
-CREATE TYPE wheelType AS ENUM(
-    'LEFT',
-    'RIGHT'
-);
 CREATE TABLE car (
     id serial PRIMARY KEY,
     model VARCHAR(64) NOT NULL,
     adNumber BIGINT NOT NULL UNIQUE ,
     yearOfRelease date NOT NULL,
     colour varchar(64),
-    wheel wheelType,
-    engine engineType,
+    wheel varchar(64),
+    engine varchar(64),
     engineVolume FLOAT,
     mileage BIGINT,
     accident BOOLEAN,
