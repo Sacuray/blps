55f5474e180a8af12ab50d4c6000200a1caa1e53
Lasat <61060252+Himezon@users.noreply.github.com>
2024-04-13 17:08:27 +0300

    init new branch

--- create_tables.sql
+++ create_tables.sql
@@ -1,15 +1,16 @@
 DROP TABLE IF EXISTS car CASCADE;
 
 CREATE TABLE car (
-    id serial PRIMARY KEY,
-    model VARCHAR(64) NOT NULL,
-    adNumber BIGINT NOT NULL UNIQUE ,
-    yearOfRelease date NOT NULL,
-    colour varchar(64),
-    wheel varchar(64),
-    engine varchar(64),
-    engineVolume FLOAT,
-    mileage BIGINT,
-    accident BOOLEAN,
-    price BIGINT
+     id serial PRIMARY KEY,
+     brand VARCHAR(64) NOT NULL,
+     model VARCHAR(64) NOT NULL,
+     adNumber BIGINT NOT NULL UNIQUE ,
+     yearOfRelease date NOT NULL,
+     colour varchar(64),
+     wheel varchar(64),
+     engine varchar(64),
+     engineVolume FLOAT,
+     mileage BIGINT,
+     accident BOOLEAN,
+     price BIGINT
 );
