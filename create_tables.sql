8848afbd5cd75590800a8e32bc0a74ded5975062
Lasat <61060252+Himezon@users.noreply.github.com>
2024-05-11 06:14:16 +0300

    Database extension

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS car CASCADE;
+DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS admin CASCADE;
+DROP TABLE IF EXISTS superadmin CASCADE;
 
+-- Создание таблицы Car
 CREATE TABLE car (
      id serial PRIMARY KEY,
      brand VARCHAR(64) NOT NULL,
@@ -13,4 +17,35 @@ CREATE TABLE car (
      mileage BIGINT,
      accident BOOLEAN,
      price BIGINT
+);
+
+-- Создание таблицы User
+CREATE TABLE user (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(100),
+    phone_number VARCHAR(20),
+    registration_date DATE,
+);
+
+-- Создание таблицы Admin
+CREATE TABLE admin (
+    admin_id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE REFERENCES User(user_id)
+);
+
+-- Создание таблицы Superadmin
+CREATE TABLE superadmin (
+    superadmin_id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE REFERENCES User(user_id)
+);
+
+-- Создание таблицы UserCar
+CREATE TABLE UserCar (
+    user_car_id SERIAL PRIMARY KEY,
+    user_id INT,
+    car_id INT,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (car_id) REFERENCES Car(car_id)
 );
